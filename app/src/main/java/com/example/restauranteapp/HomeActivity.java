d867b82bf291d41f82673ab03ffcc1fafaf95ded
Felipe Erazo <felipe.erazo3@gmail.com>
2016-10-10 17:26:11 -0300

    Layout
    Layout de la aplicacion

---
+++ app/src/main/java/com/example/restauranteapp/HomeActivity.java
@@ -0,0 +1,25 @@
+package com.example.restauranteapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    public void whatapp(View view) {
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+        sendIntent.setType("text/plain");
+        sendIntent.setPackage("com.whatsapp");
+        startActivity(sendIntent);
+    }
+
+}
